f3c294e576c120479c9654f02a86051672e0c058
Jhanuar <jhanuar.sanchez@correo.usa.edu.co>
2019-09-01 17:47:32 -0500

    se  creo la carrera quemada

--- src/main/java/com/recruiters/recruiterssupportbackEnd/model/entities/Career.java
+++ src/main/java/com/recruiters/recruiterssupportbackEnd/model/entities/Career.java
@@ -5,10 +5,35 @@
  */
 package com.recruiters.recruiterssupportbackEnd.model.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.util.StringUtils;
+
 /**
  *
  * @author Jhanuar Sanchez
  */
 public class Career {
     
+    @Id
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static boolean isCorrectForCreate(Career career) {
+        return !StringUtils.isEmpty(career.getName());
+    }
 }
