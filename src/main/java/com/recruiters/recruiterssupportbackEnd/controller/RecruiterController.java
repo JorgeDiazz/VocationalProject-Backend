039d3518fd3c0726d1e8177a4b050cb2baa3db71
seam33 <sebastian3304@gmail.com>
2019-09-14 18:42:34 -0500

    Se realizo la implementacion del CRUD para el reclutador, se elimino PersonController para dividir la informacion de reclutador y postulante

---
+++ src/main/java/com/recruiters/recruiterssupportbackEnd/controller/RecruiterController.java
@@ -0,0 +1,117 @@
+
+package com.recruiters.recruiterssupportbackEnd.controller;
+
+import com.recruiters.recruiterssupportbackEnd.controller.exceptions.UnauthorizedException;
+import com.recruiters.recruiterssupportbackEnd.controller.http.HttpResponseEntity;
+import com.recruiters.recruiterssupportbackEnd.controller.http.ResponseUtils;
+import com.recruiters.recruiterssupportbackEnd.controller.request_entities.CreateRequestRecruiter;
+import com.recruiters.recruiterssupportbackEnd.controller.request_entities.UpdateRequestRecruiter;
+import com.recruiters.recruiterssupportbackEnd.model.entities.Person;
+import com.recruiters.recruiterssupportbackEnd.model.entities.UserEntity;
+import com.recruiters.recruiterssupportbackEnd.repository.PersonRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author seam33
+ */
+@RestController
+@RequestMapping("/recruiter")
+public class RecruiterController {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public RecruiterController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET}, allowedHeaders = {"Content-Type", "Authorization"})
+
+    @GetMapping("/{nit}")
+    public ResponseEntity<List<Person>> getAllRecruiterByNit(@PathVariable String nit){
+        return HttpResponseEntity.getOKStatus(personRepository.findByNit(nit));
+    }
+    
+    
+    @PutMapping("/")
+    public ResponseEntity<Person> updateRecruiter(@RequestBody UpdateRequestRecruiter updateRequestRecruiter) throws UnauthorizedException {
+
+        String id = updateRequestRecruiter.getId();
+        String name = updateRequestRecruiter.getName();
+        String email = updateRequestRecruiter.getEmail();
+        String password = updateRequestRecruiter.getPassword();
+
+        Optional<Person> optPerson = personRepository.findById(id);
+
+        if (optPerson.isPresent()) {
+
+            Person newPerson = optPerson.get();
+
+            if (name != null && !name.isEmpty()) {
+                newPerson.setName(name);
+            }
+
+            if (email != null && !email.isEmpty()) {
+
+                if (personRepository.findByEmail(email).isPresent()) {
+                    throw new UnauthorizedException("email in use");
+                } else {
+                    newPerson.setEmail(email);
+                }
+            }
+
+            if (password != null && !password.isEmpty()) {
+                newPerson.setPassword(password);
+            }
+
+            personRepository.save(newPerson);
+
+            return HttpResponseEntity.getOKStatus(newPerson, ResponseUtils.generateTokenHeader(newPerson));
+
+        }
+
+        throw new UnauthorizedException("could not update");
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<UserEntity> createRecruiter(@RequestBody CreateRequestRecruiter createRequestRecruiter) throws Throwable {
+
+        String id=createRequestRecruiter.getId();
+        String email=createRequestRecruiter.getEmail();
+        String nit=createRequestRecruiter.getNit();
+        
+        if (personRepository.existsById(id)) { 
+            throw new UnauthorizedException("user already exist");
+        } else { 
+
+            if (personRepository.findByEmail(email).isPresent()) {
+                throw new UnauthorizedException("email in use");
+            } else {
+                Person recruiter = new Person();
+                recruiter.setNitCompany(nit);
+                recruiter.setId(id);
+                recruiter.setName("USER " + id);
+                recruiter.setEmail(email);
+                recruiter.setPassword("12345");
+                recruiter.setType(UserEntity.TYPE.RECRUITER.name());
+                personRepository.save(recruiter);
+                return HttpResponseEntity.getOKStatus(recruiter, ResponseUtils.generateTokenHeader(recruiter));
+            }
+        }
+    }
+    
+
+}
--- src/main/java/com/recruiters/recruiterssupportbackEnd/repository/PersonRepository.java
+++ src/main/java/com/recruiters/recruiterssupportbackEnd/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.recruiters.recruiterssupportbackEnd.repository;
 
 import com.recruiters.recruiterssupportbackEnd.model.entities.Person;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +10,8 @@ public interface PersonRepository extends JpaRepository<Person, String> {
 
     @Query("from Person where email = ?1")
     Optional<Person> findByEmail(String email);
+    
+    @Query("from Person where nit_company = ?1")
+    List<Person> findByNit(String nit);
     
 }
