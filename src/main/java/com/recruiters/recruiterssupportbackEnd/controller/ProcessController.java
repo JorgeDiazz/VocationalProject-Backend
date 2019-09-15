8479ef16c0172235e5b4a2ff2bf51aa44c617943
Kate Morales <katemorales@iMac-de-Kate.local>
2019-09-15 13:21:12 -0500

    cambio nombre servicio Crear Proceso

--- src/main/java/com/recruiters/recruiterssupportbackEnd/controller/ProcessController.java
+++ src/main/java/com/recruiters/recruiterssupportbackEnd/controller/ProcessController.java
@@ -48,7 +48,7 @@ public class ProcessController {
     }
      @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET}, allowedHeaders = {"Content-Type","Authorization"})
     @PostMapping("/register")
-    public ResponseEntity<Process> createRecruiter(@RequestBody CreateRequestProcess createRequestProcess) throws Throwable {
+    public ResponseEntity<Process> createProcess(@RequestBody CreateRequestProcess createRequestProcess) throws Throwable {
 
         int idJob= createRequestProcess.getId_job_position();
         String name=createRequestProcess.getName();
