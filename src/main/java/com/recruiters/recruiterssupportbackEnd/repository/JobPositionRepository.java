ff4a3ca3e429599ccf471e002a7a148b369bfd8c
Jorge Diaz <jorgeandresdn1@gmail.com>
2019-10-14 13:16:33 -0500

    Se crea servicio obtener cargos por empresa

--- src/main/java/com/recruiters/recruiterssupportbackEnd/repository/JobPositionRepository.java
+++ src/main/java/com/recruiters/recruiterssupportbackEnd/repository/JobPositionRepository.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.recruiters.recruiterssupportbackEnd.repository;
 
 import com.recruiters.recruiterssupportbackEnd.model.entities.JobPosition;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-/**
- *
- * @author katemorales
- */
 public interface JobPositionRepository extends JpaRepository<JobPosition, Integer> {
-    
+
+    @Query("from JobPosition where nit_company = ?1")
+    public List<JobPosition> findByNit(String nit);
+
 }
